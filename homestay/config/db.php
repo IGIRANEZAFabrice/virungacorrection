638b9724a3389038a8887e716c4669e53ae41cc3
IGIRANEZA FABRICE <124435619+IGIRANEZAFabrice@users.noreply.github.com>
2026-07-13 22:41:17 +0200

    db password changes

--- homestay/config/db.php
+++ homestay/config/db.php
@@ -1,8 +1,8 @@
 <?php
 $db_host = 'localhost';
-$db_user = 'root';
-$db_pass = '';
-$db_name = 'homestay';
+$db_user = 'u703364579_fab';
+$db_pass = 'Fab@11823';
+$db_name = 'u703364579_fab';
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
